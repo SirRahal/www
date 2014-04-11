c8dc109cba051bdb30e8c8d4b657821bb95051cb
Sari Rahal <biggyistheman@yahoo.com>
2014-04-11 12:53:15 -0400

    started ITi in Yii for the second go around

---
+++ protected/views/site/index.php
@@ -0,0 +1,27 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name;
+?>
+
+<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+<p>Congratulations! You have successfully created your Yii application.</p>
+
+<p>You may change the content of this page by modifying the following two files:</p>
+<ul>
+	<li>View file: <code><?php echo __FILE__; ?></code></li>
+	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+</ul>
+
+<div><!--left Col-->
+    <?php
+
+    ?>
+</div>
+<div><!--center Col-->
+
+</div>
+<div><!--right Col-->
+
+</div>
