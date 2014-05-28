12839b162fb261b9fff658d8f5dcb077782d95e6
Sari Rahal <biggyistheman@yahoo.com>
2014-05-27 21:01:17 -0400

    got the backet started

--- protected/config/main.php
+++ protected/config/main.php
@@ -18,17 +18,17 @@ return array(
 		'application.components.*',
 	),
 
-    'modules'=>array(
-        // uncomment the following to enable the Gii tool
-
-        'gii'=>array(
-            'class'=>'system.gii.GiiModule',
-            'password'=>'bjc4400',
-            // If removed, Gii defaults to localhost only. Edit carefully to taste.
-            'ipFilters'=>array('127.0.0.1','::1'),
-        ),
-
-    ),
+	'modules'=>array(
+		// uncomment the following to enable the Gii tool
+		/*
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'Enter Your Password Here',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
+		*/
+	),
 
 	// application components
 	'components'=>array(
@@ -36,16 +36,17 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
-        // uncomment the following to enable URLs in path-format
+		// uncomment the following to enable URLs in path-format
 
-        'urlManager'=>array(
-            'urlFormat'=>'path',
-            'rules'=>array(
-                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-            ),
-        ),
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
@@ -58,6 +59,7 @@ return array(
 			'password' => '',
 			'charset' => 'utf8',
 		),
+
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -8,4 +8,5 @@ $this->breadcrumbs=array(
 ?>
 <h1>About</h1>
 
-
+<p>This is a "static" page. You may change the content of this page
+by updating the file <code><?php echo __FILE__; ?></code>.</p>
