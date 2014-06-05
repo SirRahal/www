e0b821d07ba659eb173ab8dd9021c10a8cec7b91
Sari Rahal <biggyistheman@yahoo.com>
2014-06-05 10:49:04 -0400

    just added the 8th issue

---
+++ issues/8/files/assets/basic-html/javascript/page21.js
@@ -0,0 +1,35 @@
+﻿
+    pageContentLoad(
+    {
+    "currentPage":{"item":"page21", "num":"40-41"},
+    "keywords":"",
+    "fullLink":"../../../index.html",
+    "height":"792",
+    "width":" 1440",
+    "isWide":"True",
+    "bookWidth":"720",
+    "bookHeight":"792",
+
+    "download":[{}
+      ,{"pdfPublication":{"url":"publication.pdf", "size":"11.95 MB"}}
+    
+      ,{"PdfPage":{"url":"page0021.pdf", "size":"590.64 KB"}}
+     ],
+
+    
+    
+    "substrate":{"background":"url(../common/page-substrates/page0021.jpg)",
+    "backgroundSize":"contain", "backgroundColor":"#FFFFFF"},
+    
+    "leftTool":{"innerText":"38-39","src":"page20.html"},
+    
+    "rightTool":{"innerText":" 42-43","src":"page22.html"},
+    
+    "cart":[{}
+    
+    ],
+    "content":[{}
+        
+    ]
+})
+ 	
---
+++ issues/8/files/assets/basic-html/javascript/properties.js
@@ -0,0 +1,189 @@
+﻿
+    properties = {
+    "isShare":"True",
+    "isToc":"False",
+    "isCopy":"False",
+    "isThumbnail":"True",
+    "isDownload":"True"
+    }
+
+    properties.isShare = (properties.isShare == "True");
+    properties.isToc = (properties.isToc == "True");
+    properties.isCopy = (properties.isCopy == "True");
+    properties.isThumbnail = (properties.isThumbnail == "True");
+    properties.isDownload = (properties.isDownload == "True");
+    
+
+    LANG = 'en';
+    LOCALS_FOLDER = 'locals';
+    ASSETS_FOLDER = '../';
+    DOWNLOAD_FOLDER = 'common/downloads';
+    PAGES_CONTENT_FOLDER='../common/pages-content';
+    TITLE = "june_2014_digital_book";
+    COVER_SRC = "../flash/pages/page0001_s.jpg";
+    SPREAD_SRC = "../flash/spread.html";
+    FULL_SRC = "../../../index.html";
+    LINK = "_blank";
+    RTL = ("False" == "True");
+    CID = [];
+	
+	PublicationTypeName = 'digital magazine';
+	PublicationTypeUrl = 'http://flippingbook.com/online-digital-magazine';
+
+    shopProperties = {
+    id: '',
+    name: '',
+    usePublicationCart: ('false' == "true"),
+    hideZeroDecimals: ('false' == "true"),
+    currency: {
+    code: 'USD',
+        name: 'US Dollar',
+        symbol: '$',
+        thousandsDelimiter: ' ',
+        decimalSeparator: '.',
+        placeSymbolAfterPrice: ("False" == "True"),
+        accuracy: '2'
+    },
+    demoURL: 'http://flippingbook.com/help/publisher-2/docs/quick-start/ecommerce-integration/?src=demo',
+    providers: {
+    
+        'PayPal':{
+           
+            'email': ''
+        
+                ,
+            
+            'method': 'POST'
+        
+                ,
+            
+            'sandbox': 'False'
+        
+        }
+
+        }
+    };
+
+	properties.isECommerce = !(shopProperties.id == '');
+
+    cartProperties = {
+    publicationID :'f0c78bfd297154ed825c7fcd8de3a9ef',
+    scale:'',
+    hasPriceTag :'True',
+	hasProductNameOnTag :'True',
+    onClickAction:'Window',
+
+    circleColor:'#259C2B',
+    circleRadius: 16,
+    circleOrientation:'bottomright',
+    basketColor: "#fff",
+    basketEmptyImg:"./style/cart.png",
+    basketEmptyFull:"./style/cart_down.png",
+    priceTagColorAlpha:'0.8',
+    priceTagColor:'255, 255, 255',
+    priceTagFontColor:'#337F37'
+
+    };
+
+    
+
+	if ( typeof CURRENT_PAGE === 'undefined'){
+		var waitBasicJS = function(){
+			if(typeof TRANSITION_TIME !== 'undefined'){
+				var script = document.createElement('script');
+				var head = document.getElementsByTagName("head")[0];
+				var script = document.createElement('script');
+				script.type = 'text/javascript';
+				script.src = './javascript/init.js';
+				head.appendChild(script);
+
+				waitInitJS();
+			}else{
+				setTimeout(waitBasicJS, 100);
+			}
+		};
+
+		var waitInitJS = function(){
+			if(typeof quantityElem !== 'undefined'){
+
+			}else{
+				setTimeout(waitInitJS, 100);
+			}
+		};
+    
+
+		if(properties.isECommerce){
+
+			var head = document.getElementsByTagName("head")[0];
+			var link = document.createElement('link');
+			link.type = "text/css";
+			link.rel  = "stylesheet";
+			link.href = "style/cart.css";
+			head.appendChild(link);
+
+			var waitProductJS = function(){
+				if(typeof productsBD !== 'undefined'){
+					var script = document.createElement('script');
+					var head = document.getElementsByTagName("head")[0];
+					var script = document.createElement('script');
+					script.type = 'text/javascript';
+					script.src = ASSETS_FOLDER +'common/ecommerce/cart.js';
+					head.appendChild(script);
+
+					waitCartJS();
+				}else{
+					setTimeout(waitProductJS, 100);
+				}
+			};
+
+			var waitCartJS = function(){
+				if(typeof Cart !== 'undefined'){
+					var script = document.createElement('script');
+					var head = document.getElementsByTagName("head")[0];
+					var script = document.createElement('script');
+					script.type = 'text/javascript';
+					script.src = './javascript/ecommerce.js';
+					head.appendChild(script);
+
+					waitEcommerceJS();
+				}else{
+					setTimeout(waitCartJS, 100);
+				}
+			};
+
+			var waitEcommerceJS = function(){
+				if(typeof quantityElem !== 'undefined'){
+					var script = document.createElement('script');
+					var head = document.getElementsByTagName("head")[0];
+					var script = document.createElement('script');
+					script.type = 'text/javascript';
+					script.src = './javascript/basic.js';
+					head.appendChild(script);
+
+					waitBasicJS();
+				}else{
+					setTimeout(waitEcommerceJS, 100);
+				}
+			};
+
+
+			var script = document.createElement('script');
+			var head = document.getElementsByTagName("head")[0];
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = ASSETS_FOLDER +'common/ecommerce/products.js';
+			head.appendChild(script);
+
+			waitProductJS();
+
+		}else{
+		  var script = document.createElement('script');
+		  var head = document.getElementsByTagName("head")[0];
+		  var script = document.createElement('script');
+		  script.type = 'text/javascript';
+		  script.src = './javascript/basic.js';
+		  head.appendChild(script);
+
+		  waitBasicJS();
+		}
+	}
