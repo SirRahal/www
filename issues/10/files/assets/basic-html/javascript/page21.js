0a1727ced3709676c47b884ba8817dbf80e32594
Sari Rahal <biggyistheman@yahoo.com>
2014-08-04 10:20:51 -0400

    added the new issue of industrial times

---
+++ issues/10/files/assets/basic-html/javascript/page21.js
@@ -0,0 +1,35 @@
+﻿
+    pageContentLoad(
+    {
+    "currentPage":{"item":"page21", "num":"40-41"},
+    "keywords":"",
+    "fullLink":"../../../index.html",
+    "height":"792",
+    "width":" 1440",
+    "isWide":"True",
+    "bookWidth":"720",
+    "bookHeight":"792",
+
+    "download":[{}
+      ,{"pdfPublication":{"url":"publication.pdf", "size":"31.54 MB"}}
+    
+      ,{"PdfPage":{"url":"page0021.pdf", "size":"1.50 MB"}}
+     ],
+
+    
+    
+    "substrate":{"background":"url(../common/page-substrates/page0021.jpg)",
+    "backgroundSize":"contain", "backgroundColor":"#FFFFFF"},
+    
+    "leftTool":{"innerText":"38-39","src":"page20.html"},
+    
+    "rightTool":{"innerText":" 42-43","src":"page22.html"},
+    
+    "cart":[{}
+    
+    ],
+    "content":[{}
+        
+    ]
+})
+ 	
