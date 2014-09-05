0b45c40247482b96dc7f774b5ef64660c6c18f64
Sari Rahal <biggyistheman@yahoo.com>
2014-09-05 16:57:37 -0400

    added the new issue

---
+++ issues/11/files/assets/basic-html/javascript/page13.js
@@ -0,0 +1,35 @@
+﻿
+    pageContentLoad(
+    {
+    "currentPage":{"item":"page13", "num":"24-25"},
+    "keywords":"",
+    "fullLink":"../../../index.html",
+    "height":"792",
+    "width":" 1440",
+    "isWide":"True",
+    "bookWidth":"720",
+    "bookHeight":"792",
+
+    "download":[{}
+      ,{"pdfPublication":{"url":"publication.pdf", "size":"28.74 MB"}}
+    
+      ,{"PdfPage":{"url":"page0013.pdf", "size":"1.13 MB"}}
+     ],
+
+    
+    
+    "substrate":{"background":"url(../common/page-substrates/page0013.jpg)",
+    "backgroundSize":"contain", "backgroundColor":"#FFFFFF"},
+    
+    "leftTool":{"innerText":"22-23","src":"page12.html"},
+    
+    "rightTool":{"innerText":" 26-27","src":"page14.html"},
+    
+    "cart":[{}
+    
+    ],
+    "content":[{}
+        
+    ]
+})
+ 	
