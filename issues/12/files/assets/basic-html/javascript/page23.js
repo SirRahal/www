c5068dc97e7a21101db623ee1834191ead93cedc
Sari Rahal <biggyistheman@yahoo.com>
2014-10-07 16:38:26 -0400

    added the new issue

---
+++ issues/12/files/assets/basic-html/javascript/page23.js
@@ -0,0 +1,35 @@
+﻿
+    pageContentLoad(
+    {
+    "currentPage":{"item":"page23", "num":"44-45"},
+    "keywords":"",
+    "fullLink":"../../../index.html",
+    "height":"792",
+    "width":" 1440",
+    "isWide":"True",
+    "bookWidth":"720",
+    "bookHeight":"792",
+
+    "download":[{}
+      ,{"pdfPublication":{"url":"publication.pdf", "size":"31.46 MB"}}
+    
+      ,{"PdfPage":{"url":"page0023.pdf", "size":"1.55 MB"}}
+     ],
+
+    
+    
+    "substrate":{"background":"url(../common/page-substrates/page0023.jpg)",
+    "backgroundSize":"contain", "backgroundColor":"#FFFFFF"},
+    
+    "leftTool":{"innerText":"42-43","src":"page22.html"},
+    
+    "rightTool":{"innerText":" 46-47","src":"page24.html"},
+    
+    "cart":[{}
+    
+    ],
+    "content":[{}
+        
+    ]
+})
+ 	
---
+++ issues/12/files/assets/basic-html/javascript/page5.js
@@ -0,0 +1,35 @@
+﻿
+    pageContentLoad(
+    {
+    "currentPage":{"item":"page5", "num":"8-9"},
+    "keywords":"",
+    "fullLink":"../../../index.html",
+    "height":"792",
+    "width":" 1440",
+    "isWide":"True",
+    "bookWidth":"720",
+    "bookHeight":"792",
+
+    "download":[{}
+      ,{"pdfPublication":{"url":"publication.pdf", "size":"31.46 MB"}}
+    
+      ,{"PdfPage":{"url":"page0005.pdf", "size":"1.32 MB"}}
+     ],
+
+    
+    
+    "substrate":{"background":"url(../common/page-substrates/page0005.jpg)",
+    "backgroundSize":"contain", "backgroundColor":"#FFFFFF"},
+    
+    "leftTool":{"innerText":"6-7","src":"page4.html"},
+    
+    "rightTool":{"innerText":" 10-11","src":"page6.html"},
+    
+    "cart":[{}
+    
+    ],
+    "content":[{}
+        
+    ]
+})
+ 	
