4eefa904fe4724923dffebcfd4ce906cccb775bd
Sari Rahal <biggyistheman@yahoo.com>
2014-11-11 17:08:17 -0500

    ass kicking

--- index.php
+++ index.php
@@ -1,13 +1,48 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Sari
+ * Date: 11/11/14
+ * Time: 1:23 PM
+ */ ?>
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+<html>
+<head>
+    <title>eBay Search Results</title>
+</head>
+<body>
+<h1>eBay Search Results</h1>
+<div id="results"></div>
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+<script>
+    function _cb_findItemsByKeywords(root)
+    {
+        var items = root.findItemsByKeywordsResponse[0].searchResult[0].item || [];
+        var html = [];
+        html.push('<table width="100%" border="0" cellspacing="0" cellpadding="3"><tbody>');
 
-require_once($yii);
-Yii::createWebApplication($config)->run();
+        for (var i = 0; i < items.length; ++i)
+        {
+            var item     = items[i];
+            var title    = item.title;
+            var pic      = item.galleryURL;
+            var viewitem = item.viewItemURL;
+
+            if (null != title && null != viewitem)
+            {
+                html.push('<tr><td>' + '<img src="' + pic + '" border="0">' + '</td>' +
+                    '<td><a href="' + viewitem + '" target="_blank">' + title + '</a></td></tr>');
+            }
+        }
+        html.push('</tbody></table>');
+        document.getElementById("results").innerHTML = html.join("");
+    }
+</script>
+
+<!--
+Use the value of your appid for the appid parameter below.
+-->
+<script src=http://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=nuteksal-1a23-4cc6-bee4-61c507ae977f&OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&callback=_cb_findItemsByKeywords&REST-PAYLOAD&keywords=dbz%20&paginationInput.entriesPerPage=30>
+</script>
+</body>
+</html>​
