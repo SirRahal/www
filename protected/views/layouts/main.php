81de1c680455e72ba1645d52035c7a234b20b14c
Sari Rahal <biggyistheman@yahoo.com>
2014-11-21 12:06:29 -0500

    removed bootstrap

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -45,18 +45,6 @@
 
     <title><?php echo CHtml::encode($this->pageTitle); ?></title>
 
-
-    <!-- Bootstrap Core CSS -->
-    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <style>
-        body {
-            padding-top: 70px;
-            /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
-        }
-    </style>
-
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -68,37 +56,6 @@
 
 <body>
 
-<!-- Navigation -->
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Start Bootstrap</a>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li>
-                    <a href="/">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
-    </div>
-    <!-- /.container -->
-</nav>
 
 <!-- Page Content -->
 <div class="container">
@@ -233,9 +190,6 @@
 
 </div>
 </body>
-
-<!-- Bootstrap Core JavaScript -->
-<script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap.min.js"></script>
 
 </html>
 
--- protected/views/ticket/my_tickets.php
+++ protected/views/ticket/my_tickets.php
@@ -42,7 +42,7 @@ $this->breadcrumbs=array(
             dialog = $( "#dialog-form" ).dialog({
                 autoOpen: false,
                 height: 350,
-                width: 350,
+                width: 500,
                 modal: true,
                 buttons: {
                     "Add Entry": addTicket,
@@ -73,24 +73,8 @@ $this->breadcrumbs=array(
         <?php echo $this->renderPartial('/site/container/count_down'); ?>
     </div>
     <div class="clear"></div>
-<!--if the user has more than one ticket-->
-    <?php /*foreach($mytickets as $ticket){
-        $my_picks = Picks::model()->find_tickets_by_ID($ticket['ID']);
-        $string_exploded = explode("-", $ticket['code']);
-        $school_ID = $string_exploded[0];
-        $school = School::model()->get_name_by_ID($school_ID);
-        */?><!--
-        <div class="regional_div ticket ticket_box" id="my_picks" style="">
-            <span><a class="tooltip" title="See how you rank up against others in <?php /*echo $school; */?>" href="/index.php/school/<?php /*echo $school_ID; */?>"><b><?php /*echo $school;*/?></b></a></span><br/>
-            <span><b>Entry # : </b><?php /*echo $ticket['code']; */?></span>
-            <?php /*echo $this->renderPartial('container/my_picks_div', array('picks' => $my_picks,'ticket_ID' => $ticket['ID']));*/?>
-            <a  class="button tooltip ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only" style="padding: 5px 81px 5px 81px;" href="/index.php/ticket/update/<?php /*echo $ticket['ID'];*/?>" title="Edit This entry up until March 5th 12pm EST">Edit Entry</a>
-        </div>
-    --><?php /*}*/?>
 <button id="add_ticket">Add Entry</button>
     <div class="clear"></div>
-
-
     <div id="accordion">
     <?php
         $mytickets = Ticket::model()->get_tickets_by_user_ID();
@@ -102,14 +86,12 @@ $this->breadcrumbs=array(
             </div>
         <?php } ?>
     </div>
-
-
 <div id="dialog-form" title="Add a Entry">
     <p class="validateTips">Please Enter Entry Number</p>
     <form>
         <fieldset>
             <label for="ticket_code">Entry Code #</label>
-            <input type="text" name="ticket_code" id="ticket_code" placeholder="000-0000" class="text ui-widget-content ui-corner-all" title="You can find this on the bottom right hand side of your entry">
+            <input type="text" name="ticket_code" id="ticket_code" placeholder="0-0000" class="text ui-widget-content ui-corner-all" title="You can find this on the bottom right hand side of your entry">
             <img src="/images/faq-ticket-codes.png" width="150";/>
             <input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
         </fieldset>
