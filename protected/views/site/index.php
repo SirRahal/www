221eed4f72bdb4378fffbb216050571bd7edbc9f
Sari Rahal <biggyistheman@yahoo.com>
2014-12-05 17:30:01 -0500

    added a funny image

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,6 +6,27 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to Nu Tek Listings</h1>
 
-<h3>Today's activity</h3>
+<h3>This Site is for Nu Tek Listers Only.<br/>If you are not a lister, please leave.</h3>
 
-<h3>Yesterday's activity</h3>
+<img style="float: right; " src="/images/alien.gif"/>
+<br/>
+<h6>Back Listers</h6>
+<ul>
+    <li>Create listings</li>
+    <li>Edit Listings</li>
+    <li>Copy Listings</li>
+</ul>
+<br/>
+<h6>eBay Listers</h6>
+<ul>
+    <li>Special Views</li>
+    <li>Mark Items as Ebay Listed</li>
+    <li>Mark Items as Sold</li>
+</ul>
+<br/>
+<h6>Admin</h6>
+<ul>
+    <li>Create Users</li>
+    <li>Mark Items as Ebay Listed</li>
+    <li>View User Activity</li>
+</ul>
