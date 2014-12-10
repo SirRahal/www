bf614aded6f753492dc361de392c5be4ae50ccb6
Sari Rahal <biggyistheman@yahoo.com>
2014-12-10 15:49:30 -0500

    put everything back

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -4,12 +4,28 @@
 
 $this->pageTitle=Yii::app()->name . ' - Error';
 $this->breadcrumbs=array(
-	'Error',
+    'Error',
 );
 ?>
+<?php if( $code == 404) { ?>
+    <div style="text-align: center;">
+        <img src="/images/mario-404.gif" style="width: 80%;"/>
+        <h1>Click <a href="/index.php">HERE</a> to go home </h1>
+    </div>
+<?php
+}elseif( $code == 403){ ?>
+    <div style="text-align: center;">
+        <img src="/images/wizard-403.gif" style="width: 80%;"/>
+        <h6>Click <a href="/index.php">HERE</a> to go home</h6>
+        <h6>If you need access to this page, ask the Admin to give you permissions to it.</h6>
+    </div>
 
-<h2>Error <?php echo $code; ?></h2>
+<?php }else { ?>
 
-<div class="error">
-<?php echo CHtml::encode($message); ?>
-</div>
+    <h2>Error <?php echo $code; ?></h2>
+
+    <div class="error">
+        <?php echo CHtml::encode($message); ?>
+    </div>
+
+<?php } ?>
--- upload.php
+++ upload.php
@@ -4,10 +4,6 @@ $output_dir = "images/uploads/";
 if(isset($_FILES["myfile"]))
 {
 
-    $url = $_SERVER['REQUEST_URI'];
-    $exploded_url = explode('/',$url);
-    $listing_ID = end($exploded_url);
-
     $ret = array();
 
     $error =$_FILES["myfile"]["error"];
@@ -23,20 +19,20 @@ if(isset($_FILES["myfile"]))
             $ImageExt = substr($ImageName, strrpos($ImageName, '.'));
             $ImageExt       = str_replace('.','',$ImageExt);
             $ImageName      = preg_replace("/\.[^.\s]{3,4}$/", "", $ImageName);
-            $NewImageName = $ImageName.'-'.$RandomNum.'.'.$ImageExt;
+            $NewImageName = $listing_ID.'_'.$ImageName.'-'.$RandomNum.'.'.$ImageExt;
 
             move_uploaded_file($_FILES["myfile"]["tmp_name"],$output_dir. $NewImageName);
             //echo "<br> Error: ".$_FILES["myfile"]["error"];
 
             $ret[$fileName]= $output_dir.$NewImageName;
 
-            $image_entry = new Images;
+
+            $image_entry = new Images();
             $image_entry->image = $NewImageName;
-            $image_entry->listing_ID = $listing_ID;
+            $image_entry->listing_ID = 1;
             if (!$image_entry->save()) {
                 print_r($image_entry->getErrors());
             }
-
         }
         else
         {
@@ -51,14 +47,14 @@ if(isset($_FILES["myfile"]))
                 $ImageExt = substr($ImageName, strrpos($ImageName, '.'));
                 $ImageExt       = str_replace('.','',$ImageExt);
                 $ImageName      = preg_replace("/\.[^.\s]{3,4}$/", "", $ImageName);
-                $NewImageName = $ImageName.'-'.$RandomNum.'.'.$ImageExt;
+                $NewImageName = $listing_ID.'_'.$ImageName.'-'.$RandomNum.'.'.$ImageExt;
 
                 $ret[$NewImageName]= $output_dir.$NewImageName;
                 move_uploaded_file($_FILES["myfile"]["tmp_name"][$i],$output_dir.$NewImageName );
 
-                $image_entry = new Images;
+                $image_entry = new Images();
                 $image_entry->image = $NewImageName;
-                $image_entry->listing_ID = $listing_ID;
+                $image_entry->listing_ID = 1;
                 if (!$image_entry->save()) {
                     print_r($image_entry->getErrors());
                 }
