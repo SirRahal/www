9e42c878d27c54678564d3585a32245ac93c6b16
Sari Rahal <biggyistheman@yahoo.com>
2014-12-13 13:45:45 -0500

    worked on the admin side

--- protected/views/user/admin.php
+++ protected/views/user/admin.php
@@ -12,53 +12,54 @@ $this->menu=array(
 	array('label'=>'Create User', 'url'=>array('create')),
 );
 
-Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$('#user-grid').yiiGridView('update', {
-		data: $(this).serialize()
-	});
-	return false;
-});
-");
 ?>
 
 <h1>Manage Users</h1>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
+<!-- DataTables CSS -->
+<link rel="stylesheet" type="text/css" href="/css/jquery.dataTables.css">
+<!-- DataTables -->
+<script type="text/javascript" charset="utf8" src="/js/jquery.dataTables.js"></script>
 
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'user-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'ID',
-		'first_name',
-		'last_name',
-		'email',
-		'city',
-		'state',
-		/*
-		'zip',
-		'phone',
-		'user_name',
-		'password',
-		*/
-		array(
-			'class'=>'CButtonColumn',
-		),
-	),
-)); ?>
+<script>
+    $(document).ready( function () {
+        $('#table_id').DataTable();
+    } );
+</script>
+<br/>
+<?php
+$users = User::model()->findAll();
+
+?>
+<table id="table_id">
+    <thead>
+    <tr>
+        <td>ID</td>
+        <td>First</td>
+        <td>Last</td>
+        <td>Email</td>
+        <td>Tickets</td>
+        <td></td>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach($users as $user){ ?>
+        <tr>
+            <!--ID-->
+            <td><?php echo $user['ID'];?></td>
+            <!---->
+            <td><?php echo $user['first_name'];?></td>
+            <!---->
+            <td><?php echo $user['last_name'];?></td>
+            <!---->
+            <td><?php echo $user['email'];?></td>
+            <!---->
+            <td><?php echo $user['phone'];?></td>
+            <!---->
+            <td><a href="update/<?php echo $user['ID']; ?>">Edit</a></td>
+    <?php } ?>
+
+    </tbody>
+</table>
+
