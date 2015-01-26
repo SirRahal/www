74a0b73287ebf0f7eabde38a7e170623ca0fe7bf
Sari Rahal <biggyistheman@yahoo.com>
2015-01-26 06:23:02 -0500

    fresh start

--- contact/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-// To
-define("WEBMASTER_EMAIL", 'info@midwaymachinerymovers.com');
-?>
--- insurance.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Sari
- * Date: 1/12/15
- * Time: 6:24 PM
- */ ?>
